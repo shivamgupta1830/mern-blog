8bc95b0bbc0191641d75cce037a0a0d3ee5c7502
Shivam Gupta <shivamgupta1830@gmail.com>
2024-09-01 21:48:59 +0530

    basic client set-up

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const appRouter = createBrowserRouter[{}];
+
+function App() {
+  return (
+    <div>
+      <h1>Hi</h1>
+    </div>
+  );
+}
+
+export default App;
