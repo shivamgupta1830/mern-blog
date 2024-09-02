1e1eadd347d3b3bd4db3c2bf265eb31ec1b80081
Shivam Gupta <shivamgupta1830@gmail.com>
2024-09-02 12:23:25 +0530

    Database connected

--- api/index.js
+++ api/index.js
@@ -1,4 +1,15 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+const connectDB = (uri) => {
+  mongoose
+    .connect(uri)
+    .then(() => console.log("DB Connected"))
+    .catch((err) => console.log(err));
+};
+connectDB(process.env.MONGO_URI);
 
 const app = express();
 
