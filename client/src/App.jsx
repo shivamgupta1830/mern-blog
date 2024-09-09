ea680ff40ca3bc46ee03a24a75dfddbe2b0617c8
Shivam Gupta <shivamgupta1830@gmail.com>
2024-09-09 12:13:45 +0530

    footer component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import SignIn from "./pages/SignIn";
 import Projects from "./pages/Projects";
 import Error from "./pages/Error";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 import Home from "./pages/Home";
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <Header />
 
       <Outlet />
+      <Footer />
     </div>
   );
 }
