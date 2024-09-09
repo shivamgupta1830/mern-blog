a3fa5ebebf1c73c702a84a3811b42b35204bd2f6
Shivam Gupta <shivamgupta1830@gmail.com>
2024-09-09 12:35:23 +0530

    sign in functionality

--- api/routes/authRoute.js
+++ api/routes/authRoute.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { signup } from "../controllers/authController.js";
+import { signin, signup } from "../controllers/authController.js";
 
 const authRouter = express.Router();
 
 authRouter.post("/signup", signup);
+authRouter.post("/signin", signin);
 
 export default authRouter;
